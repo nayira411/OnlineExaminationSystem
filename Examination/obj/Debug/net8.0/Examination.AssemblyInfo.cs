ccf75aa0e4c39b005768e53dadedfcd623c96c21
BothinaMesalem <bothinamesalem@gmail.com>
2024-03-20 01:10:02 +0200

    bothina2

--- Examination/obj/Debug/net8.0/Examination.AssemblyInfo.cs
+++ Examination/obj/Debug/net8.0/Examination.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examination")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4881646ac949dd7f298f5fa4ed7c20acf3705f39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c6dfebd9f79d9772a0ea8a0cbe3a0e778b20c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examination")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Examination")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
