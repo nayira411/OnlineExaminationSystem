928ab7cdfed8412c12d401d140b26e4a1b8c0067
esraashabana <104281057+esraashabana@users.noreply.github.com>
2024-03-24 22:12:54 +0200

    exam

---
+++ Examination/Controllers/UpcommingExamsController.cs
@@ -0,0 +1,29 @@
+﻿using Examination.Repo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examination.Controllers
+{
+	public class UpcommingExamsController : Controller
+	{
+		//this will be from auth data
+		int StdId = 4;
+		TeacherFormRepo repo = new TeacherFormRepo();
+		public IActionResult Index()
+		{
+			var StdTrack = repo.GetTrackIdOfStudent(StdId);
+			var StdCourses = repo.GetStudentCourses(StdId);
+			List<int> coursesIds = new List<int>();
+			foreach (var Courses in StdCourses)
+			{
+				coursesIds.Add(Courses.CrId);
+			}
+			var data = repo.HaveExam(StdTrack, coursesIds);
+			foreach (var Courses in data)
+			{
+                Console.WriteLine(Courses);
+            }
+		    ViewBag.exam= data;
+			return View();
+		}
+	}
+}
