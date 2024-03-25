4dfa9fe9d018c0e3a571a0ecbddc1892270e6beb
donyamohamed <donyaelhol56@gmail.com>
2024-03-25 21:02:20 +0200

    Reports

--- Examination/Models/CourseViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Examination.Models
-{
-	public class CourseViewModel
-	{
-		public Course course { get; set; }
-		List<Instructor> instructors { get; set; }
-		public TrackCourse trackCourse { get; set; }
-	}
-}
--- Examination/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Examination.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
