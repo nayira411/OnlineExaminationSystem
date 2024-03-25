f8f0499d1b89580defbc8515be9aca1af2353b99
esraashabana <104281057+esraashabana@users.noreply.github.com>
2024-03-25 05:06:19 +0200

    some changes

---
+++ Examination/Controllers/AdminDasbordController.cs
@@ -0,0 +1,25 @@
+﻿using Examination.Models;
+using Examination.Repo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examination.Controllers
+{
+	public class AdminDasbordController : Controller
+	{
+		AdminDashbordRepo Repo=new AdminDashbordRepo();
+		public IActionResult Index()
+		{
+			ViewBag.stdCount=Repo.getStdsCount();
+			ViewBag.insCount=Repo.getInsCount();
+			ViewBag.crsCount=Repo.getCoursesCount();
+			ViewBag.getTracks=Repo.getTracks();
+			ViewBag.GetTracksCount = Repo.GetTracksCount();
+            var viewModel = new ChartViewModel
+            {
+                Labels = Repo.getTracks().ToArray(),
+                Data = Repo.GetTracksCount().ToArray()
+            };
+            return View(viewModel);
+		}
+	}
+}
---
+++ Examination/Repo/AdminDashbordRepo.cs
@@ -0,0 +1,48 @@
+﻿using Examination.Models;
+
+namespace Examination.Repo
+{
+    public class AdminDashbordRepo
+    {
+        ExamContext context = new ExamContext();
+        public int getStdsCount()
+        {
+            var res = context.Students.Count();
+            return res;
+        }
+        public int getInsCount()
+        {
+            var res = context.Instructors.Count();
+            return res;
+        }
+        public int getCoursesCount()
+        {
+            var res = context.Courses.Count();
+            return res;
+        }
+        public List<string> getTracks()
+        {
+            List<string> tNames= new List<string>();
+            var res = context.Tracks.ToList(); 
+            foreach ( var track in res)
+            {
+                tNames.Add(track.Tname);
+            }
+            return tNames;
+        }
+        public List<int> GetTracksCount()
+        {
+            var tracksCount = new List<int>();
+            var tracks = context.Tracks.ToList();
+
+            foreach (var track in tracks)
+            {
+                var count = track.Students.Count;
+                tracksCount.Add(count);
+            }
+
+            return tracksCount;
+        }
+
+    }
+}
